8b79137d9e07689915f563463947554c524bbf69
liuxy <391861737@qq.com>
2019-12-15 19:31:24 +0800

    Disallow the partial match in `WcMatch`
    The WcMatch will allow the partial match when the pattern is a plain
    text without an asterisk in it. Apparently it is not the expected
    behaviour.

--- util/wildcard.go
+++ util/wildcard.go
@@ -2,6 +2,10 @@ package util
 
 // WcMatch checks if the given sample string matchs the given pattern
 func WcMatch(sample string, pattern string) bool {
+	if pattern == "*" {
+		return true
+	}
+
 	samp, patt := []rune(sample), []rune(pattern)
 	slen, plen := len(samp), len(patt)
 	asterisk, pos := -1, 0 // the index of the `*` and the position of rune in the sample
@@ -12,15 +16,20 @@ func WcMatch(sample string, pattern string) bool {
 		if pos < plen && patt[pos] == '*' {
 			asterisk = pos // remember the asterisk's position
 			pos++          // check next
-		} else if pos < plen && (patt[pos] == '?' || patt[pos] == s) { // when it is `?` or matchs the current sample rune
+		} else if pos < plen && (patt[pos] == '?' || patt[pos] == s) {
+			// when it is `?` or matchs the current sample rune
 			pos++ // check next
 		} else if asterisk != -1 {
 			// if can't match the pattern rune after the asterisk, just let the pattern rune
 			// stay and move back the sample cursor
 			i += asterisk - pos + 1
 			pos = asterisk + 1
+		} else if pos == plen {
+			// if already consumed all the runes of the pattern, yet the sample is still not finished
+			// and there's no asterisk at all, then increase the pos so that the match will fail
+			pos++
 		} else {
-			continue
+			break
 		}
 	}
 	for ; pos < plen && patt[pos] == '*'; pos++ {
